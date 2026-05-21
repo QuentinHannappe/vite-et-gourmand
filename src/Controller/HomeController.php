d0c38f34130118fb6af812e99f260aacc2539c52
QuentinHannappe <nithpe@gmail.com>
2026-05-21 09:02:32 +0200

    avis accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\AvisRepository;
+
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(AvisRepository $avisRepository): Response
     {
+        $avis = $avisRepository->findBy(['statut' => 'valide']);
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'avis' => $avis,
         ]);
     }
 }
